a40f8a103c7b22d59f5cf5dc184566703adcdae9
Szymon Zolopa <shymo1977@gmail.com>
2021-08-26 15:39:19 +0200

    AddClassActive

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ function titleClickHandler(event){
   /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = clickedElement.querySelector('href');
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
