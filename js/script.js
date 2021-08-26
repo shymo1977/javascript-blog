660f6b4f92bb4c75e7b61142b0d63ac6240ec719
Szymon Zolopa <shymo1977@gmail.com>
2021-08-26 15:11:25 +0200

    GetHrefAttribute

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
+  event.preventDefault();
+  clickedElement.classList.add('active');
   console.log('Link was clicked!', event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     }
   
-  /* add class 'active' to the clicked link */
+  /*  [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+
+  /*  [DONE]  remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 
@@ -21,7 +26,7 @@ function titleClickHandler(event){
     }
 
   /* get 'href' attribute from the clicked link */
-
+  const articleSelector = clickedElement.getAttribute('href');
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
